bab79a4a7dcb6615ab3bab7a90819b6c09a3ffe5
karaccet <31154929+karaccet@users.noreply.github.com>
2017-08-20 00:18:22 +0400

    Create beautiful-soup-example.py

---
+++ beautiful-soup-example.py
@@ -0,0 +1,13 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+#Retrieve all the anchor tages
+tags = soup('a')
+
+for tag in tags:
+    print(tag.get('href', None))
