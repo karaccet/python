be8fa2ca4955152278282e71c013321b5bf9d873
karaccet <31154929+karaccet@users.noreply.github.com>
2017-08-19 14:54:19 +0400

    True updated

--- web-call.py
+++ web-call.py
@@ -8,7 +8,7 @@ mysock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
 mysock.send(cmd)
 
-while TRUE:
+while True:
 	data = mysock.recv(512)
 	if len(data) < 1: 
 		break
