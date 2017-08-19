19b744f0011fbbef58f54ac615c78f41d34a59d2
karaccet <31154929+karaccet@users.noreply.github.com>
2017-08-19 14:38:14 +0400

    Updates

--- web-call.py
+++ web-call.py
@@ -1,3 +1,6 @@
 import socket
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('services.groupkt.com/country/get/iso2code/IN', 80))
+#Google.com - 216.58.209.132
+##services.groupkt.com/country/get/iso2code/IN - rest service to pull country details
+mysock.connect(('216.58.209.132', 80))
+mysock.close()
